1c0e4f62b9fd869f86930c0c0576c830e6ebad0b
Pond <69410473+pondps123@users.noreply.github.com>
2023-03-22 18:25:18 +0700

    Create backend.py

---
+++ Backend/backend.py
@@ -0,0 +1,108 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import mysql.connector
+
+#-----ดึงข้อมูลจากเว็บ-----
+def database(web):
+    page = requests.get(web)
+    page.content
+
+    soup_for_ProvinceName = BeautifulSoup(page.content, 'html.parser')
+    content_for_ProvinceName = soup_for_ProvinceName.find_all('h1')
+    content_for_ProvinceName = str(content_for_ProvinceName)
+
+    RegularExpression_pattern_ProvinceName = r'รายชื่อวัดใน(.*)(</span>)'
+    ProvinceName = re.search(
+        RegularExpression_pattern_ProvinceName, content_for_ProvinceName)
+    ProvinceName = ProvinceName.group(1)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    content = soup.find_all('li')
+    content = str(content)
+
+    RegularExpression_pattern = r'title="(วัด.*?)"'
+    TempleName_list = re.findall(RegularExpression_pattern, content)
+
+    RegularExpression_pattern_for_delete_addition = r' (.*)'
+    new_temple_name = re.sub(
+        RegularExpression_pattern_for_delete_addition, '', '\n'.join(TempleName_list))
+
+    TempleName_list = new_temple_name.split('\n')
+
+    if ProvinceName == "จังหวัดชัยภูมิ":
+        TempleName_list = list(map(lambda x: x.replace(
+            'วัดวังอุดม(', 'วัดวังอุดม'), TempleName_list))
+
+
+    RegularExpression_pattern_for_delete_Not_temple = r'วัดไทย'
+    search_notTemple = re.search(
+        RegularExpression_pattern_for_delete_Not_temple, '\n'.join(TempleName_list))
+
+    index_notTemple = TempleName_list.index(search_notTemple.group())
+
+    count_for_PopList = len(TempleName_list)-index_notTemple
+
+    for i in range(count_for_PopList):
+        del_notTemple = TempleName_list.pop()
+
+    if ProvinceName == "จังหวัดชลบุรี":   
+        RegularExpression_pattern_for_drop_duplicate_temple = r'วัดคลองพลู'
+        search_duplicateTemple = re.search(
+            RegularExpression_pattern_for_drop_duplicate_temple, '\n'.join(TempleName_list))
+        index_duplicateTemple = TempleName_list.index(search_duplicateTemple.group())
+        drop_duplicateTemple = TempleName_list.pop(index_duplicateTemple)
+
+    return TempleName_list, ProvinceName
+
+#-----สร้าง CSV-----
+def writeCSV(data, name):
+    with open(f'วัดใน{name}.csv', "w", encoding='utf-8') as f:
+        for TempleName in data:
+            f.write(TempleName + "\n")
+
+#-----สร้าง database และ add ข้อมูล-----
+def addData(data, name):
+    db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Pondps123",
+    database="test"
+    )
+    mycursor = db.cursor()
+
+    table_name = changeName(name)
+    columns = "id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(45)"
+
+    mycursor.execute("CREATE TABLE IF NOT EXISTS " + table_name + " (" + columns + ")")
+    mycursor.execute("TRUNCATE TABLE "+ table_name)
+    i=1
+    for row in data:
+        mycursor.execute("INSERT INTO "+ table_name + " (id,NAME) VALUES (%s, %s)",[i,row])
+        db.commit()
+        i+=1
+    print(i-1,row) #print last temple
+    mycursor.close()
+    db.close()
+
+#-----เปลี่ยนชื่อจากภาษาไทยเป็นภาษาอังกฤษ-----
+def changeName(name):
+    if name == "จังหวัดฉะเชิงเทรา":
+        return "Chachoengsao_Temple"
+    elif name == "จังหวัดชัยนาท":
+        return "Chainat_Temple"
+    elif name == "จังหวัดชัยภูมิ":
+        return "Chaiyaphum_Temple"
+    elif name == "จังหวัดชลบุรี":
+        return "Chonburi_Temple"
+    
+#-----main-----
+data1, name1 = database("https://th.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%99%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%89%E0%B8%B0%E0%B9%80%E0%B8%8A%E0%B8%B4%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%A3%E0%B8%B2")
+data2, name2 = database("https://th.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%99%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%8A%E0%B8%B1%E0%B8%A2%E0%B8%99%E0%B8%B2%E0%B8%97")
+data3, name3 = database("https://th.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%99%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%8A%E0%B8%B1%E0%B8%A2%E0%B8%A0%E0%B8%B9%E0%B8%A1%E0%B8%B4")
+data4, name4 = database("https://th.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%99%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%8A%E0%B8%A5%E0%B8%9A%E0%B8%B8%E0%B8%A3%E0%B8%B5")
+
+addData(data1, name1)
+addData(data2, name2)
+addData(data3, name3)
+addData(data4, name4)
