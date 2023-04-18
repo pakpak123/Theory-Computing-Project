ac88b58b7e2d8ed1ecc349b0bf47ae70b2f45a34
Tnp_Sr <69105766+Tnp-Sr@users.noreply.github.com>
2023-04-18 17:45:02 +0700

    แก้โค้ด
    -แปลงตัวแปรhidden เป็นlist
    -เช็คจังหวัดจากที่เลือกแล้วprintบนterminal

--- web_crawler/main.py
+++ web_crawler/main.py
@@ -4,64 +4,73 @@ import pandas as pd
 
 app = Flask(__name__)
 
+def Convert(string):
+    li = list(string.split(","))
+    return li
+
 @app.route("/")
-def home(): 
-    return render_template("index.html")
+#--------- แสดงข้อมูลจาก csv
+def home():
+    l = []
+    data = "Hi"
+    path = 'D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\sample_data.csv'
+    l.append("วัดในฉะเชิงเทรา")
+    with open(path,encoding='utf8') as file:
+        reader = csv.reader(file)
+        for e in reader:
+            l.append(e)
+        # print(l)
+        return render_template("index.html",data=data, csv=l)
+# def home(): 
+#     return render_template("index.html")
     
 
 #--------- สร้างเงื่อนไขให้แสดงข้อมูล
 
 
-# @app.route("/",methods=["POST","GET"])
-# def index(): 
-#     province_names = ["Chachoengsao","Chainat","Chaiyaphum","Chonburi"]
-#     path_provinces = {
-#         "Chachoengsao" : "D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\data\Chachoengsao_Temple.csv","Chainat" : "D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\data\Chainat_Temple.csv",
-#         "Chaiyaphum" : "D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\data\Chaiyaphum_Temple.csv","Chonburi": "D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\data\Chonburi_Temple.csv"
-#     }
+@app.route("/list_add",methods=["POST","GET"])
+def list_add(): 
+    province_names = ["Chachoengsao","Chainat","Chaiyaphum","Chonburi"]
+    path_provinces = {
+        "Chachoengsao" : "D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\data\Chachoengsao_Temple.csv",
+        "Chainat" : "D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\data\Chainat_Temple.csv",
+        "Chaiyaphum" : "D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\data\Chaiyaphum_Temple.csv",
+        "Chonburi": "D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\data\Chonburi_Temple.csv"
+    }
     
-#     if request.method == 'POST':
-#         hidden_provinces = request.form['hidden_provinces']
-#         msg = 'New record created successfully'+ hidden_provinces
+    if request.method == 'POST':
+        hidden_provinces = request.form['hidden_provinces']
+        msg = 'New record created successfully => '+ hidden_provinces
 
-#         temple_names = []
+        # แปลง hidden_provinces เป็น list
+        hidden_provinces = hidden_provinces.split(",")
 
-#         # if(hidden_provinces in province_names):
-#         for province in hidden_provinces:
-#             temple_names.append(province)
-#             with open(path_provinces[province],encoding='utf8') as file:
-#                 reader= csv.reader(file)
-#                 for e in reader:
-#                     temple_names.append(e)
-#                 # print(l)
-#             return render_template("index.html", province=province ,csv=temple_names)
+        print(hidden_provinces)
+        temple_names = []
+
+        for province in hidden_provinces:
+            temple_names.append([province])
+            with open(path_provinces[province],encoding='utf8') as file:
+                reader= csv.reader(file)
+                for e in reader:
+                    temple_names.append(e)
+                # print(l)
+        print(temple_names)
+        return jsonify(msg)
+        # return render_template("index.html", province=province ,csv=temple_names)
         
-#         # else:
-#         #     return render_template("index.html")
+        # else:
+        #     return render_template("index.html")
 
 
 #--------- เก็บค่าจาก multi-option ไว้ใน hidden_provinces
 
-@app.route("/ajax_add",methods=["POST","GET"])
-def ajax_add():
-    if request.method == 'POST':
-        hidden_provinces = request.form['hidden_provinces']
-        msg = 'New record created successfully -> '+ hidden_provinces
-    return jsonify(msg)
-
- 
-#--------- แสดงข้อมูลจาก csv
-
-# def home():
-#     l = []
-#     data = "Hi"
-#     path = 'D:\EDUCATION\KMITL\Study\Y3_t2_2022\Theory\Web_Crawler\Theory-Computing-Project\web_crawler\sample_data.csv'
-#     with open(path,encoding='utf8') as file:
-#         reader = csv.reader(file)
-#         for e in reader:
-#             l.append(e)
-#         # print(l)
-#         return render_template("index.html",data=data, csv=l)
+# @app.route("/ajax_add",methods=["POST","GET"])
+# def ajax_add():
+#     if request.method == 'POST':
+#         hidden_provinces = request.form['hidden_provinces']
+#         msg = 'New record created successfully -> '+ hidden_provinces
+#     return jsonify(msg)
 
 
 
